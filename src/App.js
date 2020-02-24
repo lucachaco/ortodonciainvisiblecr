529616d5751f90275c380fff1069f2f0b3deeb4c
lucachaco <luiscarlosch@gmail.com>
2020-02-23 22:27:05 -0600

    feat: add copy

--- src/App.js
+++ src/App.js
@@ -5,20 +5,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Ortodoncia Invisiblecr
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <body data-gr-c-s-loaded="true"><p class="p1"><span class="s1"><b> ¿Qué es la Ortodoncia Invisible?</b></span></p>
+        <p class="p1"><span class="s1">Es un tratamiento de ortodoncia que funciona por medio de alineadores dentales “invisibles” o transparentes, o sea sin brackets. Estos alineadores se cambian periódicamente según lo indique el ortodoncista. De esta manera los dientes se van moviendo progresivamente conforme se van cambiando los alineadores progresivamente. </span></p>
+        <p class="p1"><span class="s1"><b>Ventajas:</b></span></p>
+        <ul class="ul1">
+            <li class="li2"><span class="s1">&nbsp;Confort</span></li>
+            <ul class="ul1">
+                <li class="li2"><span class="s1">Los tratamientos con brackets tienen la desventaja que nos pueden lastimar los tejidos blandos de la boca. Esto no sucede con los alineadores dentales, ya que al ser de un material plástico y suave no es posible que lesione los tejidos. Al ser movimientos tan controlados el dolor es casi nulo, contrario a los bracktes.</span></li>
+            </ul>
+            <li class="li2"><span class="s1">&nbsp;Estética</span></li>
+            <ul class="ul1">
+                <li class="li2"><span class="s1">Por algo es llamada ortodoncia invisible, en realidad podemos decir que son imperceptibles, ya que de cerca otra persona podría notar el plástico. Sin embargo en comparación con los tratamientos de ortodoncia convencionales, estos alineadores presentan la mayor estética del mercado. </span></li>
+            </ul>
+            <li class="li2"><span class="s1">&nbsp;Velocidad del tratamiento</span></li>
+            <ul class="ul1">
+                <li class="li2"><span class="s1">La mayoría de casos resuelven en menos tiempo que la ortodoncia tradicional, ya que el cambio de alineadores se realiza cada quince días, mientras que con la ortodoncia tradicional hacemos los ajustes cada mes.</span></li>
+            </ul>
+        </ul>
+        <p class="p3">&nbsp;</p>
+        <p class="p1"><span class="s1"><b>¿Es efectivo?</b></span></p>
+        <p class="p1"><span class="s1">100% efectivo ya que todo el tratamiento se planifica de forma computarizada haciendo completamente predecibles todos los movimientos dentales que se van a realizar.</span></p>
+        <p class="p1"><span class="s1"><b>¿Quiénes aplican?</b></span></p>
+        <p class="p1"><span class="s1">Todo paciente que aplique para un tratamiento de ortodoncia regular aplica para ortodoncia invisible.</span></p>
+        <p class="p1"><span class="s1"><b>¿Cuál es el proceso para obtener este tratamiento?</b></span></p>
+        <p class="p1"><span class="s1">Se realiza un escaneo de los dientes del paciente, el cual se ingresa a un software donde se realiza la simulación del movimiento de los dientes hasta dejarlos completamente alineados. Esta simulación se envía al ortodoncista, quien a su vez aprobará o solicitará los cambios que considere necesarios. El paciente puede ver esta simulación en su teléfono celular o computadora, es decir podrá ver el resultado antes de empezar.</span></p>
+        <p class="p1"><span class="s1">Posterior a esto se envían a fabricar los alineadores en un material diseñado especialmente para este fin, unos días después su ortodoncista los recibirá y le hará entrega de los mismos progresivamente con el fin de darle seguimiento a su tratamiento hasta lograr los resultados deseados.</span></p>
+        <p class="p1"><span class="s1">Aunque los alineadores se cambia cada dos semanas, las citas con el ortodoncista son mensuales, quiere decir que el paciente cambiará los alineadores quincenales en su casa.</span></p>
+        <p class="p1"><span class="s1"><b>¿Cuál es el costo?</b></span></p>
+        <p class="p1"><span class="s1">El costo varía según la cantidad de alineadores que requiera la persona para corregir su situación. Puede ir desde algo muy sencillo de $500 hasta tratamientos muy complejos de $3800, la mayoría de casos se encuentran en el rango de los $1500-$2500. Todos los tratamientos cuentan con financiamiento durante el tiempo de tratamiento.</span></p>
+        <p class="p4"><span class="s1">Ortodoncia sin brackets con excelentes resultados a corto plazo.</span></p>
+        <p class="p6"><span class="s1"><b>Dr. Víctor Arguedas Sánchez</b></span></p>
+        <p class="p6"><span class="s1">El Dr. Víctor Arguedas Sánchez, es Licenciado en Odontología y Doctor en cirugía dental, graduado de la Universidad de Costa Rica en el año 2009, posteriormente obtiene su especialidad en Ortodoncia y Ortopedia funcional de los maxilares, destacándose con graduación de honor en su generación. Se desempeña como docente e investigador de la Facultad de Odontología de la Universidad de Costa Rica, desde el año 2010-2017. Cuenta con diferentes certificaciones, dentro de las cuales destacan: Certificación en técnica Invisalign y Certificación en terapia miofuncional. Cuenta con su práctica privada desde el 2010, año en el que funda <a href="https://www.arguedassanchezdental.com/">Arguedas Sánchez Centro Odontológico</a>, una de las clínicas más grandes y reconocidas en la ciudad de Alajuela, en esta clínica se agendan apoximadamente 1000 citas mensuales, y cuenta con un personal humano de 12 colaboradores. Posteriormente también participa en sociedad en la clínica Sabana Dental Care. Actualmente es uno de los ortodoncistas que más ha impulsado el novedoso tratamiento de ortodoncia invisible, con una gran cantidad de casos tratados.</span></p>
+        </body>
     </div>
   );
 }
