805df003d68b511da8f97af03ffa15508c7bc51d
lucachaco <luiscarlosch@gmail.com>
2020-02-23 22:34:31 -0600

    Updates

---
+++ precache-manifest.29f0c114ddf550aa4d4f501fc36e8cc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c190e5eee74ebea3d8336819b87c42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec70850f6299ed35a94b",
+    "url": "/static/css/main.7f527e1e.chunk.css"
+  },
+  {
+    "revision": "f9ae65d8d8a5845de228",
+    "url": "/static/js/2.e4f269b8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e4f269b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec70850f6299ed35a94b",
+    "url": "/static/js/main.7d40b557.chunk.js"
+  },
+  {
+    "revision": "7334e40ec7dd9b9798d6",
+    "url": "/static/js/runtime-main.1417d64c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ortodonciainvisiblecr/precache-manifest.a68b0407a384cddccc1121325b86ecaa.js"
+  "/precache-manifest.29f0c114ddf550aa4d4f501fc36e8cc0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ortodonciainvisiblecr/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
